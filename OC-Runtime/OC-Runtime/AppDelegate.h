70160bb27934c0241665251421b2749c3889d9c1
Sniper <sniper@ZW-LMtekiMac-mini.local>
2015-12-10 23:54:04 +0800

    init

---
+++ OC-Runtime/OC-Runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OC-Runtime
+//
+//  Created by Sniper on 15/12/10.
+//  Copyright © 2015年 ShengQiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OC-Runtime/OC-Runtime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OC-Runtime
+//
+//  Created by Sniper on 15/12/10.
+//  Copyright © 2015年 ShengQiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
